cda17f5cc62ff83cf836c37632c71dfdefef8da1
HamsalekhaP <hamsa2231994@gmail.com>
2020-12-05 21:48:09 -0600

    fix map position and integrate

--- app.js
+++ app.js
@@ -162,87 +162,81 @@ function updateBarChart(r_data) {
 d3.select("#mySlider").on("change", function() {
   yearOfView = this.value
   rain_title.text("Rainfall Distribution in year " + yearOfView + " in " + country_id_map[selected_country]);
-  
+
   rainfallDataProcessing(true)
 
   selectedValue = this.value
   yearOfView = selectedValue
   queue()
-  .defer(d3.json, "world_countries.json")
-  .defer(d3.csv, "country_avg_temp.csv")
-  .await(ready);
+    .defer(d3.json, "world_countries.json")
+    .defer(d3.csv, "country_avg_temp.csv")
+    .await(ready);
 });
 // Code for Rainfall graph ends here
 
-var tip = d3.tip()
+var map_tip = d3.tip()
   .attr('class', 'd3-tip s')
   .offset(function(d) {
     if (d.properties.name === "Russia") {
-// Set tooltips
+      // Set tooltips
       return [-10, 200]
     } else return [-10, 0]
   })
   .html(function(d) {
-    if (isNaN(d.temperature)){
-      return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Temperature: </strong><span class='details'>" 
-    + format(d.temperature) + "<br></span>" + "<strong>Tempertature compared to 1991: </strong><span class='details'>" + format(d.difference) + "<br></span>";
-    }else{
-      return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Temperature: </strong><span class='details'>" 
-    + format(d.temperature) + "°C <br></span>" + "<strong>Tempertature compared to 1991: </strong><span class='details'>" + format(d.difference) + "°C <br></span>";
-    }    
+    if (isNaN(d.temperature)) {
+      return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Temperature: </strong><span class='details'>" +
+        format(d.temperature) + "<br></span>" + "<strong>Tempertature compared to 1991: </strong><span class='details'>" + format(d.difference) + "<br></span>";
+    } else {
+      return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Temperature: </strong><span class='details'>" +
+        format(d.temperature) + "°C <br></span>" + "<strong>Tempertature compared to 1991: </strong><span class='details'>" + format(d.difference) + "°C <br></span>";
+    }
   })
-var margin = { top: 0, right: 0, bottom: 0, left: 0 },
-  width = 900 - margin.left - margin.right,
-  height = 720 - margin.top - margin.bottom;
-var color = d3.scaleThreshold()
-     .domain([-100, -1, -0.5, -0.1, 0, 0.1, 1, 1.5, 2, 2.5])
-     .range(["rgb(255,255,255)", "rgb(66,146,198)", "rgb(123,169,201)", "rgb(198,219,239)", "rgb(250,240,230)", "rgb(255,204,153)", "rgb(255,178,102)", "rgb(255,128,0)", "rgb(255,0,0)", "rgb(204,0,0)"])
+var map_margin = { top: 0, right: 0, bottom: 0, left: 0 },
+  width = 750 - map_margin.left - map_margin.right,
+  height = 720 - map_margin.top - map_margin.bottom;
+var map_color = d3.scaleThreshold()
+  .domain([-100, -1, -0.5, -0.1, 0, 0.1, 1, 1.5, 2, 2.5])
+  .range(["rgb(255,255,255)", "rgb(66,146,198)", "rgb(123,169,201)", "rgb(198,219,239)", "rgb(250,240,230)", "rgb(255,204,153)", "rgb(255,178,102)", "rgb(255,128,0)", "rgb(255,0,0)", "rgb(204,0,0)"])
 var path = d3.geoPath();
-var svg = d3.select(".map-panel")
+var map_svg = d3.select(".map-wrapper")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
   .append('g')
   .attr('class', 'map');
-var projection = d3.geoMercator().scale(100)
+var projection = d3.geoMercator().scale(90)
   .translate([width / 2, height / 2.1]);
 var path = d3.geoPath().projection(projection);
-svg.call(tip);
+map_svg.call(map_tip);
 queue()
   .defer(d3.json, "world_countries.json")
   .defer(d3.csv, "country_avg_temp.csv")
   .await(ready);
 
-var g = svg.append("g")
+var map_g = map_svg.append("g")
   .attr("class", "legendThreshold")
   .attr("transform", "translate(20,20)");
-g.append("text")
+map_g.append("text")
   .attr("class", "caption")
   .attr("x", 0)
   .attr("y", -6)
   .text("Temperature difference (°C)");
-var labels = ['No data','<-1', '-1~-0.5', '-0.5~-0.1', '-0.1~0.1', '0.1~0.5', '0.5~1', '1~1.5', '1.5~2', '>2'];
-var legend = d3.legendColor()
-  .labels(function (d) { return labels[d.i]; })
+var map_label = ['No data', '<-1', '-1~-0.5', '-0.5~-0.1', '-0.1~0.1', '0.1~0.5', '0.5~1', '1~1.5', '1.5~2', '>2'];
+var map_legend = d3.legendColor()
+  .labels(function(d) { return map_label[d.i]; })
   .shapePadding(4)
-  .scale(color);
-svg.select(".legendThreshold")
-  .call(legend);
-// d3.select("#mySlider").on("change", function() {
-//   selectedValue = this.value
-//   yearOfView = selectedValue
-//   queue()
-//   .defer(d3.json, "world_countries.json")
-//   .defer(d3.csv, "country_avg_temp.csv")
-//   .await(ready);
-// })
+  .scale(map_color);
+map_svg.select(".legendThreshold")
+  .call(map_legend);
 
 function filterCriteria(d) {
   return d.year === yearOfView;
 }
-function startTempFilter(d){
+
+function startTempFilter(d) {
   return d.year === '1991';
 }
+
 function ready(error, data, temperature) {
   var startTemp = temperature.filter(startTempFilter);
   var filteredTemp = temperature.filter(filterCriteria);
@@ -254,24 +248,24 @@ function ready(error, data, temperature) {
   data.features.forEach(function(d) { d.temperature = temperatureById[d.id] });
 
   startTemp.forEach(function(d) {
-    if (isNaN(temperatureById[d.id])){
+    if (isNaN(temperatureById[d.id])) {
       differences[d.id] = -100;
-    }else{
+    } else {
       differences[d.id] = (temperatureById[d.id] - startTemps[d.id]).toFixed(3);
     }
   });
   data.features.forEach(function(d) { d.difference = differences[d.id] });
-  svg.append("g")
+  map_svg.append("g")
     .attr("class", "countries")
     .selectAll("path")
     .data(data.features)
     .enter().append("path")
     .attr("d", path)
-    .style("fill", function(d) { 
-      if (typeof differences[d.id] === "undefined"){
+    .style("fill", function(d) {
+      if (typeof differences[d.id] === "undefined") {
         return "rgb(255,255,255)";
-      }else{
-        return color(differences[d.id]);
+      } else {
+        return map_color(differences[d.id]);
       }
     })
     .style('stroke', 'white')
@@ -282,24 +276,24 @@ function ready(error, data, temperature) {
     .style('stroke-width', 0.3)
     .on('mouseover', function(d) {
       if (d.geometry.coordinates[0][0][0][1] <= 0) {
-        tip.direction('n').show(d);
+        map_tip.direction('n').show(d);
       } else {
-        tip.direction('s').show(d);
-      }     
+        map_tip.direction('s').show(d);
+      }
       d3.select(this)
         .style("opacity", 1)
         .style("stroke", "white")
         .style("stroke-width", 3);
-      })
+    })
     .on('mouseout', function(d) {
-      tip.hide(d);
- 
+      map_tip.hide(d);
+
       d3.select(this)
         .style("opacity", 0.8)
         .style("stroke", "white")
         .style("stroke-width", 0.3);
     }).attr("transform", "scale(1)"); // determine if it would be better to have larger map later
-  svg.append("path")
+  map_svg.append("path")
     .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
     .attr("class", "names")
     .attr("d", path);
